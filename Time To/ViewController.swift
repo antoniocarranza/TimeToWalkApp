77ab39fc853761f1ebd08feefb63e2dd43a0e838
Antonio Carranza López <antonio.carranza@me.com>
2017-10-30 09:32:38 +0100

    Initial Commit

---
+++ Time To/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Time To
+//
+//  Created by Antonio Carranza on 30/10/17.
+//  Copyright © 2017 Antonio Carranza. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
